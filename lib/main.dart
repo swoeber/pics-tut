ed765af49cf18964102caae6421e77d2859cb327
Scott Woeber <swoeber@omegafi.com>
2019-11-24 16:24:12 -0500

    Updated for tut

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,6 @@
 import 'package:flutter/material.dart';
+import 'src/app.dart';
 
 void main() {
-  var app = MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("Let's see some images!"),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => print('pressed!'),
-        child: Icon(Icons.add),
-      ),
-    ),
-  );
-
-  runApp(app);
+  runApp(App());
 }
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
-
+class App extends StatefulWidget {
   @override
+  State<StatefulWidget> createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return App;
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Let's see some images!"),
+        ),
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 20),
+              child: Text(
+                "Text on screen $counter",
+                style: TextStyle(fontSize: 22.0),
+              ),
+            )
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              counter += 1;
+            });
+          },
+          child: Icon(Icons.add),
+        ),
+      ),
+    );
   }
 }
